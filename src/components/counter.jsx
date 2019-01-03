151d5526d316cf49507f7283762090b48d93ec68
Padam Bhandari <Padam.Bhandari@datascan.com>
2019-01-03 17:40:25 -0600

    Apply bootstrap styles to a element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        imageUrl:'https://picsum.photos/200'
+        count: 0
+        
     };
 
     render() {
         return (
-            <div>
-                <img src={this.state.imageUrl} alt=""/>>
-                <span>{this.formatCount()}</span>
-                <button>Counter</button>
+            <div>                
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Counter</button>
             </div>
         );
     }
