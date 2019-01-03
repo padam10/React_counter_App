02dca671ac9b240b7e900d9b276f9a4df416aa5c
Padam Bhandari <Padam.Bhandari@datascan.com>
2019-01-03 17:36:39 -0600

    Setting Attributes , Added random image dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {}
+    state = {
+        count: 0,
+        imageUrl:'https://picsum.photos/200'
+    };
+
     render() {
         return (
-            <React.Fragment>
-                <h1>Hello World</h1>
+            <div>
+                <img src={this.state.imageUrl} alt=""/>>
+                <span>{this.formatCount()}</span>
                 <button>Counter</button>
-            </React.Fragment>);
+            </div>
+        );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? <h1>Zero</h1> : count;
     }
 }
 export default Counter;
